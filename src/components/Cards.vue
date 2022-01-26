621174a8fc8e13041c1ddecc72671074c7d2df25
dariomontani <dariomontani@gmail.com>
2022-01-26 17:30:11 +0100

    cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,31 +1,31 @@
 <template>
     <div class="cards">
-        <div class="container">
+        <div class="container first">
             <div class="eye">
             <i class="fas fa-eye" style="font-size: 2em; z-index: 999; color: white;"></i>
             </div>
-            <img class="first" src="..\assets\img\h3-img-1.jpg" alt="">
+            <img src="..\assets\img\h3-img-1.jpg" alt="">
             <div class="background"></div>
         </div>
-        <div class="container">
+        <div class="container center">
             <div class="eye">
             <i class="fas fa-eye" style="font-size: 2em; z-index: 999; color: white;"></i>
             </div>
-            <img class="center" src="..\assets\img\h3-img-2.jpg" alt="">
+            <img src="..\assets\img\h3-img-2.jpg" alt="">
             <div class="background"></div>
         </div>
-        <div class="container">
+        <div class="container center">
             <div class="eye">
             <i class="fas fa-eye" style="font-size: 2em; z-index: 999; color: white;"></i>
             </div>
-            <img class="center" src="..\assets\img\h3-img-3.jpg" alt="">
+            <img src="..\assets\img\h3-img-3.jpg" alt="">
             <div class="background"></div>
         </div>
-        <div class="container">
+        <div class="container last">
             <div class="eye">
             <i class="fas fa-eye" style="font-size: 2em; z-index: 999; color: white;"></i>
             </div>
-            <img class="last" src="..\assets\img\h3-img-4.jpg" alt="">
+            <img src="..\assets\img\h3-img-4.jpg" alt="">
             <div class="background"></div>
         </div>
     </div>
@@ -64,27 +64,27 @@ export default {
                 border: 1px solid black;
                 position: absolute;
                 top: 0;
-                width: 355px;
-                height: 250px;
+                width: 349px;
+                height: 240px;
                 background: linear-gradient(90deg, black (8px - 1px), transparent 1%) center, linear-gradient(black (8px - 1px), transparent 1%) center,#EBEBDE;
                 filter: invert(1);
                 background-size: 8px 8px;
                 display: none;
                 
             }
-            .first{
-                padding-top: 0.3em;
-                padding-bottom: 0.3em;
-                padding-right: 0.3em;
-            }
-            .center{
-                padding: 0.3em;
-            }
-            .last{
-                padding-top: 0.3em;
-                padding-bottom: 0.3em;
-                padding-left: 0.3em;
-            }
+        }
+        .first{
+            margin-top: 0.3em;
+            margin-bottom: 0.3em;
+            margin-right: 0.3em;
+        }
+        .center{
+            margin: 0.3em;
+        }
+        .last{
+            margin-top: 0.3em;
+            margin-bottom: 0.3em;
+            margin-left: 0.3em;
         }
         .container:hover .background{
             display: block;
