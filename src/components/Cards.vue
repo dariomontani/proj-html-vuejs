218138bcc9fcabe8a8131e7fa9b335fbea521d1a
dariomontani <dariomontani@gmail.com>
2022-01-26 15:36:59 +0100

    hover

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,10 +1,34 @@
 <template>
-  <div class="cards">
-      <img class="first" src="..\assets\img\h3-img-1.jpg" alt="">
-      <img class="center" src="..\assets\img\h3-img-2.jpg" alt="">
-      <img class="center" src="..\assets\img\h3-img-3.jpg" alt="">
-      <img class="last" src="..\assets\img\h3-img-4.jpg" alt="">
-  </div>
+    <div class="cards">
+        <div class="container">
+            <div class="eye">
+            <i class="fas fa-eye" style="font-size: 2em; z-index: 999; color: white;"></i>
+            </div>
+            <img class="first" src="..\assets\img\h3-img-1.jpg" alt="">
+            <div class="background"></div>
+        </div>
+        <div class="container">
+            <div class="eye">
+            <i class="fas fa-eye" style="font-size: 2em; z-index: 999; color: white;"></i>
+            </div>
+            <img class="center" src="..\assets\img\h3-img-2.jpg" alt="">
+            <div class="background"></div>
+        </div>
+        <div class="container">
+            <div class="eye">
+            <i class="fas fa-eye" style="font-size: 2em; z-index: 999; color: white;"></i>
+            </div>
+            <img class="center" src="..\assets\img\h3-img-3.jpg" alt="">
+            <div class="background"></div>
+        </div>
+        <div class="container">
+            <div class="eye">
+            <i class="fas fa-eye" style="font-size: 2em; z-index: 999; color: white;"></i>
+            </div>
+            <img class="last" src="..\assets\img\h3-img-4.jpg" alt="">
+            <div class="background"></div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -17,21 +41,55 @@ export default {
     .cards{
         display: flex;
         justify-content: space-between;
-        img{
+        .container{
             width: calc(100% / 4);
+            position: relative;
+            .eye{
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                background-color: #EE2852;
+                padding: 2em;
+                border-radius: 50%;
+                z-index: 99;
+                font-size: 0.8em;
+                display: none;
+            }
+            img{
+                width: 100%;
+            }
+            .background{
+                border: 1px solid black;
+                position: absolute;
+                top: 0;
+                width: 355px;
+                height: 250px;
+                background: linear-gradient(90deg, black (8px - 1px), transparent 1%) center, linear-gradient(black (8px - 1px), transparent 1%) center,#EBEBDE;
+                filter: invert(1);
+                background-size: 8px 8px;
+                display: none;
+                
+            }
+            .first{
+                padding-top: 0.3em;
+                padding-bottom: 0.3em;
+                padding-right: 0.3em;
+            }
+            .center{
+                padding: 0.3em;
+            }
+            .last{
+                padding-top: 0.3em;
+                padding-bottom: 0.3em;
+                padding-left: 0.3em;
+            }
         }
-        .first{
-            padding-top: 0.3em;
-            padding-bottom: 0.3em;
-            padding-right: 0.3em;
+        .container:hover .background{
+            display: block;
         }
-        .center{
-            padding: 0.3em;
-        }
-        .last{
-            padding-top: 0.3em;
-            padding-bottom: 0.3em;
-            padding-left: 0.3em;
+        .container:hover .eye{
+            display: block;
         }
     }
 </style>
--- src/components/People.vue
+++ src/components/People.vue
@@ -1,9 +1,45 @@
 <template>
     <div class="people">
-        <img src="..\assets\img\h1-team-1a-700x700.jpg" alt="">
-        <img src="..\assets\img\h1-team-2a.jpg" alt="">
-        <img src="..\assets\img\h1-team-3a.jpg" alt="">
-        <img src="..\assets\img\h1-team-4a.jpg" alt="">
+        <div class="container">
+            <div class="containerHover">
+                <h2>FRANK BAILEY</h2>
+                <p class="arial">Kitchen Porter</p>
+                <div>
+                    <i class="fab fa-instagram-square" style="margin: 0.5em;"></i><i class="fab fa-twitter" style="margin: 0.5em;"></i><i class="fab fa-facebook-f" style="margin: 0.5em;"></i>
+                </div>
+            </div>
+            <img src="..\assets\img\h1-team-1a-700x700.jpg" alt="">
+        </div>
+        <div class="container">
+            <div class="containerHover">
+                <h2>FRANK BAILEY</h2>
+                <p class="arial">Kitchen Porter</p>
+                <div>
+                    <i class="fab fa-instagram-square" style="margin: 0.5em;"></i><i class="fab fa-twitter" style="margin: 0.5em;"></i><i class="fab fa-facebook-f" style="margin: 0.5em;"></i>
+                </div>
+            </div>
+            <img src="..\assets\img\h1-team-2a.jpg" alt="">
+        </div>
+        <div class="container">
+            <div class="containerHover">
+                <h2>FRANK BAILEY</h2>
+                <p class="arial">Kitchen Porter</p>
+                <div>
+                    <i class="fab fa-instagram-square" style="margin: 0.5em;"></i><i class="fab fa-twitter" style="margin: 0.5em;"></i><i class="fab fa-facebook-f" style="margin: 0.5em;"></i>
+                </div>
+            </div>
+            <img src="..\assets\img\h1-team-3a.jpg" alt="">
+        </div>
+        <div class="container">
+            <div class="containerHover">
+                <h2>FRANK BAILEY</h2>
+                <p class="arial">Kitchen Porter</p>
+                <div>
+                    <i class="fab fa-instagram-square" style="margin: 0.5em;"></i><i class="fab fa-twitter" style="margin: 0.5em;"></i><i class="fab fa-facebook-f" style="margin: 0.5em;"></i>
+                </div>
+            </div>
+            <img src="..\assets\img\h1-team-4a.jpg" alt="">
+        </div>
         <img class="pizzaLogo" src="..\assets\img\svg-4.svg" alt=""> 
     </div>
 </template>
@@ -18,8 +54,36 @@ export default {
     .people{
         position: relative; 
         display: flex;
-        img{
+        .container{
+            position: relative;
             width: calc(100% / 4);
+            img{
+                width: 100%;
+            }
+            .containerHover{
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                background-color: #EE2852;
+                border: 20px solid white;
+                color: white;
+                width: 100%;
+                height: 100%;
+                line-height: 2em;
+                display: none;
+                .arial{
+                    font-family: Arial, Helvetica, sans-serif;
+                    font-size: 0.8em;
+                }
+            }
+        }
+        .container:hover .containerHover{
+            display: flex;
         }
         .pizzaLogo{
             position: absolute;
